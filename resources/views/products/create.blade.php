fdf34ffc52c8a3b1afe6055d378aca81d20548cb
Code with Luis <codewithluis11@gmail.com>
2022-05-24 23:39:49 -0600

    Slug demo

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{route('products.store')}}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="name" class="required">Nombre</label>
+                            <input type="text" name="name" id="name" class="form-control {{$errors->has('name') ? 'is-invalid' : ''}}" value="{{old('name', '')}}">
+                            @if ($errors->has('name'))
+                            <span class="text-danger">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                        <div class="row d-print-none mt-2">
+                            <div class="col-12 text-right">
+                                <button class="btn btn-success" type="submit">
+                                    <i class="fa fa-fw fa-lg fa-check-circle"></i>Guardar
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    <a href="{{ route('products.create') }}" class="btn btn-primary mb-3">Nuevo Producto</a>
+
+                    <table class="table table-bordered" id="products_table">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Slug</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($products as $product)
+                            <tr>
+                                <td>{{$product->name}}</td>
+                                <td>{{$product->slug}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
